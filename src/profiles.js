2bb3118ffe37c4f807195fa62dfbad3315aadfe8
Ozan Güneş <ozangunes2894@gmail.com>
2024-06-16 00:56:28 +0300

    dummy profiles data added for development

---
+++ src/profiles.js
@@ -0,0 +1,27 @@
+export const profiles = [
+    {
+      id: 7,
+      name: "Lawson",
+      avatar: "https://reqres.in/img/faces/7-image.jpg"
+    },
+    {
+      id: 8,
+      name: "Lawson",
+      avatar: "https://reqres.in/img/faces/8-image.jpg"
+    },
+    {
+      id: 9,
+      name: "Lawson",
+      avatar: "https://reqres.in/img/faces/9-image.jpg"
+    },
+    {
+      id: 10,
+      name: "Lawson",
+      avatar: "https://reqres.in/img/faces/10-image.jpg"
+    },
+    {
+      id: 11,
+      name: "Lawson",
+      avatar: "https://reqres.in/img/faces/11-image.jpg"
+    },
+  ];
